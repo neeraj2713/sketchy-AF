7c298e2eaaa4509a2bccad12fee4873ae02f0bbb
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-07-03 16:50:07 +0530

    feat(http-backend, ws-backend): refactor JWT_SECRET import and add validation schemas

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,34 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+
+  const data = CreateUserSchema.safeParse(req.body);
+  if(!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    })
+    return;
+  }
+
   res.json({
     userId: "123"
   })
 })
 
 app.post("/signin", (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if(!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    })
+    return;
+  }
+
   const userId = 1;
 
   const token = jwt.sign({
@@ -25,6 +43,14 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room", (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if(!data.success) {
+    res.json({
+      message: "Incorrect inputs"
+    })
+    return;
+  }
+
   res.json({
     roomId: "123"
   })
