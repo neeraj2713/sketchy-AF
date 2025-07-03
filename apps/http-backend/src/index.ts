02de2c8d47ba50d4476b4125b56f64908086cb3c
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2025-07-03 15:58:59 +0530

    feat(http-backend): implement JWT authentication and add middleware for token verification

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,35 @@
 import express from "express";
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "./config";
 
 const app = express();
 
-app.listen(3000);
+app.post("/signup", (req, res) => {
+  res.json({
+    userId: "123"
+  })
+})
+
+app.post("/signin", (req, res) => {
+  const userId = 1;
+
+  const token = jwt.sign({
+    userId
+  }, JWT_SECRET);
+
+  res.json({
+    token
+  })
+
+
+})
+
+app.post("/room", (req, res) => {
+  res.json({
+    roomId: "123"
+  })
+})
+
+
+app.listen(3002);
 
